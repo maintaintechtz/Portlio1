898a48a552e3a179d07fe4808092c3838425ba3a
Ally Diah <maintaintechtz@gmail.com>
2023-12-03 01:02:35 +0300

    final

--- index.js
+++ index.js
@@ -25,8 +25,15 @@ let navLinks=document.querySelectorAll('header nav a');
                 links.classList.remove('active');
                 document.querySelector(`header nav a[href*=${id}]`).classList.add('active');
             })
+            //active section for animation on scroll
+            sec.classList.add('show-animate');
         }
-    })
+        //animation that repeates itself
+        else{
+            sec.classList.remove('show-animate');
+        }
+        
+    });
     //stick heaader
     let header = document.querySelector('header');
     header.classList.toggle('sticky',window.scrollY>100);
